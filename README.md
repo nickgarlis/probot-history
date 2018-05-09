291e2535b81ba9eced132a8a1be561db22a21b59
nickgarlis <nickgarlis@gmail.com>
2018-05-09 21:26:27 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Probot: History
+
+> a GitHub App built with [Probot](https://github.com/probot/probot) notifies contributors when a change in history has occured.
---
+++ index.js
@@ -0,0 +1,28 @@
+const getConfig = require('probot-config')
+const History = require('./lib/history')
+
+module.exports = async robot => {
+  robot.on('push', notifyUsers)
+
+  async function notifyUsers (context) {
+    if (!context.payload.forced) {
+      return
+    }
+
+    const history = await forRepository(context)
+    await history.notifyUsers()
+  }
+
+  async function forRepository (context) {
+    let config = await getConfig(context, 'history.yml')
+
+    if (!config) {
+      // Don't actually perform for repository without a config
+      config = {perform: false}
+    }
+
+    config = Object.assign(config, context.repo({logger: robot.log}))
+
+    return new History(context.github, config)
+  }
+}
